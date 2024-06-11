66738b0157265190461e34d08e26ec22c6a7cb11
lucasens1 <lucasensini1@gmail.com>
2024-06-11 18:32:50 +0200

    Setupped correctly Controller, Table and Route

--- app/Http/Controllers/HomePageController.php
+++ app/Http/Controllers/HomePageController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Train;
 use Illuminate\Http\Request;
 
 class HomePageController extends Controller
 {
     //
+    public function index(){
+        $trains = Train::all();
+        dd($trains);
+        return view('home',compact('trains'));
+    }
 }
